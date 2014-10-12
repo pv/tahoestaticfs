8706f2ba80e80d04399fd7893428323f75ca5f45
Pauli Virtanen <pav@iki.fi>
2014-10-12 19:56:00 +0300

    Reset log level

--- tahoestaticfs/cli.py
+++ tahoestaticfs/cli.py
@@ -24,7 +24,7 @@ from tahoestaticfs.version import __version__
 fuse.fuse_python_api = (0, 2)
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
 
     usage = __doc__.strip()
     usage += "".join(fuse.Fuse.fusage.splitlines(1)[2:])
